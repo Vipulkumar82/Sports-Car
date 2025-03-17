3e824d60d4ca0e08dbe44e32d9188d06036c3148
vipulkumar82 <vipulkumar212003@gmail.com>
2025-03-18 00:38:04 +0530

    nav finalised

--- script.js
+++ script.js
@@ -2,5 +2,7 @@
 
 function toggleMenu(){
     const menu = document.querySelector(".menu");
+    const nav = document.querySelector(".nav")
     menu.classList.toggle('active');
+    nav.classList.toggle('active');
 }
