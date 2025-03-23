c3cdabce5da88a6d84c3b15ce1e07e08d938de8c
vipulkumar82 <vipulkumar212003@gmail.com>
2025-03-23 17:09:59 +0530

    added video crousel in home Section

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function toggleMenu(){
     menu.classList.toggle('active');
     nav.classList.toggle('active');
 }
+
+// Change the bg video when click on gallery images
+function changeVideo(name){
+    const bgVideoList = document.querySelectorAll(".bg-video");
+    const trailers = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)){
+            video.classList.add('active');
+        }
+    });
+}
